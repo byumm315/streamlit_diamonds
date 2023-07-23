a38c18d2000db3df48d094579862bd834a3c2302
박유미 <81951313+byumm315@users.noreply.github.com>
2023-07-23 16:29:33 +0900

    Update Bar Plots.py

--- pages/Bar Plots.py
+++ pages/Bar Plots.py
@@ -4,6 +4,7 @@ import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 import numpy as np
+
 diamond_df=pd.read_csv('diamonds.csv')
 diamond_df=diamond_df.drop('Unnamed: 0',axis=1) #Unnamed: 0 변수 삭제하기
 diamond_df=diamond_df.drop(diamond_df[(diamond_df["x"]==0)|(diamond_df["y"]==0)|(diamond_df["z"]==0)].index).reset_index(drop=True) #x, y, z 셋 중 하나라도 0인 행은 삭제하였다.
@@ -16,7 +17,10 @@ diamond_df = diamond_df[(diamond_df["y"]<30)]
 diamond_df = diamond_df[(diamond_df["z"]<30)&(diamond_df["z"]>2)]
 
 df = diamond_df.drop(['cut','color','clarity'],axis=1)
-
+import random
+random.seed(90)
+diamond_df=diamond_df.loc[random.sample(list(range(len(df))),5000)].reset_index(drop=True)
+df=df.loc[random.sample(list(range(len(df))),5000)].reset_index(drop=True)
 st.subheader('The Bar plots of Diamond data')
 v1_list = ['cut','color','clarity']
 vari1 = st.selectbox(label = "Choose a First Variable", options = v1_list,key=4)
