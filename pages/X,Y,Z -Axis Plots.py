4c82f4cf0411906efaac5e1ea7e6074071e9e088
박유미 <81951313+byumm315@users.noreply.github.com>
2023-07-23 14:10:25 +0900

    Update and rename X,Y,Z -Axis Plots.py to XGBoost Regressor Model

--- pages/X,Y,Z -Axis Plots.py
+++
@@ -1,8 +0,0 @@
-# Make copy to avoid changing original data 
-label_data = data.copy()
-
-# Apply label encoder to each column with categorical data
-label_encoder = LabelEncoder()
-for col in object_cols:
-    label_data[col] = label_encoder.fit_transform(label_data[col])
-label_data.head()
