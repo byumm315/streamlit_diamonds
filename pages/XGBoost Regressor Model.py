d8ef7a748e93ed39644baec4c0ba4c29a76d1a06
박유미 <81951313+byumm315@users.noreply.github.com>
2023-07-23 17:07:32 +0900

    Update XGBoost Regressor Model.py

--- pages/XGBoost Regressor Model.py
+++ pages/XGBoost Regressor Model.py
@@ -102,7 +102,7 @@ import joblib
 
 # Title
 st.header("Streamlit Machine Learning App")
-information = st.text_input(label="X values", value="caret,depth,table,x,y,z")
+information = st.text_input(label="X values", value="0,0,0,0,0,0")
 st.write('caret: 0-10')
 st.write('depth: 40-100')
 st.write('table: 40-100')
@@ -110,25 +110,24 @@ st.write('x-axis: 0-20')
 st.write('y-axis: 0-20')
 st.write('z-axis: 0-20')
 
-value=information.split(',')[0]
-value1=information.split(',')[1]
-value2=information.split(',')[2]
-value3=information.split(',')[3]
-value4=information.split(',')[4]
-value5=information.split(',')[5]
+value=int(information.split(',')[0])
+value1=int(information.split(',')[1])
+value2=int(information.split(',')[2])
+value3=int(information.split(',')[3])
+value4=int(information.split(',')[4])
+value5=int(information.split(',')[5])
 
-if value!='caret':
-    st.write(f'Your input is caret:{value},depth:{value1},table:{value2},x:{value3},y:{value4},z:{value5}')
+st.write(f'Your input is caret:{value},depth:{value1},table:{value2},x:{value3},y:{value4},z:{value5}')
 
-v1_list = list(set(diamon_df.cut))
+v1_list = list(set(diamond_df.cut))
 vari1 = st.selectbox(label = "Choose a Cut Variable", options = v1_list,key=111)
 st.write(f"The Diamond Cut is {vali1}.")
 
-v2_list = list(set(diamon_df.color))
+v2_list = list(set(diamond_df.color))
 vari2 = st.selectbox(label = "Choose a Color Variable", options = v2_list,key=222)
 st.write(f"The Diamond Color is {vari2}.")
 
-v3_list = list(set(diamon_df.clarity))
+v3_list = list(set(diamond_df.clarity))
 vari3 = st.selectbox(label = "Choose a Clarity Variable", options = v3_list,key=333)
 st.write(f"The Diamond clarity is {vali3}.")
 st.write(f'Your input is cut: {vari1},color: {vari2},clarity: {vari3}')
