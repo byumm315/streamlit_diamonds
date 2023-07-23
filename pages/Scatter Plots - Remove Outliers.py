53680fa085528826a0e9c87fb98ba3c3036bb12d
박유미 <81951313+byumm315@users.noreply.github.com>
2023-07-23 16:19:12 +0900

    Update Scatter Plots - Remove Outliers.py

--- pages/Scatter Plots - Remove Outliers.py
+++ pages/Scatter Plots - Remove Outliers.py
@@ -31,13 +31,17 @@ def func3():
 func3()
 
 v_list = list(df.columns)
-vari = st.selectbox(label = "Choose a Variable", options = v_list,key=1)
+vari = st.selectbox(label = "Choose a Variable", options = v_list,key=0)
 diamond_df=diamond_df.sort_values(by=vari).reset_index(drop=True)
 a,b = st.select_slider(f'Choose a {vari}-axis point', options=sorted(diamond_df[vari]),value=(np.min(sorted(list(set(diamond_df[vari])))),np.max(sorted(list(set(diamond_df[vari]))))))
 k1=list(diamond_df[diamond_df[vari]==a].index)[0]
 k2=list(diamond_df[diamond_df[vari]==b].index)[-1]
-
-
-fig1 = px.scatter(diamond_df.loc[k1:k2], x=vari1, y=vari2, color=vari3,title=title)
+v1_list = list(df.columns)
+vari1 = st.selectbox(label = "Choose a First Variable", options = v1_list,key=1)
+v2_list = list(df.columns)
+vari2 = st.selectbox(label = "Choose a Second Variable", options = v2_list,key=2)
+v3_list = ['cut','color','clarity']
+vari3 = st.selectbox(label = "Choose a Third Variable", options = v3_list,key=3)
+fig1 = px.scatter(diamond_df.loc[k1:k2], x=vari1, y=vari2, color=vari3,title=f'The Scatter plots of Diamond data({vari1,vari2,vari3})'))
 fig1.update_traces(marker={'size':10})
 st.plotly_chart(fig1)
