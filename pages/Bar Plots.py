0dd27891f3daa1c75be935220e8ede5a08d311be
박유미 <81951313+byumm315@users.noreply.github.com>
2023-07-23 16:33:53 +0900

    Update Bar Plots.py

--- pages/Bar Plots.py
+++ pages/Bar Plots.py
@@ -9,6 +9,10 @@ diamond_df=pd.read_csv('diamonds.csv')
 diamond_df=diamond_df.drop('Unnamed: 0',axis=1) #Unnamed: 0 변수 삭제하기
 diamond_df=diamond_df.drop(diamond_df[(diamond_df["x"]==0)|(diamond_df["y"]==0)|(diamond_df["z"]==0)].index).reset_index(drop=True) #x, y, z 셋 중 하나라도 0인 행은 삭제하였다.
 df = diamond_df.drop(['cut','color','clarity'],axis=1)
+import random
+random.seed(90)
+diamond_df=diamond_df.loc[random.sample(list(range(len(df))),5000)].reset_index(drop=True)
+df=df.loc[random.sample(list(range(len(df))),5000)].reset_index(drop=True)
 
 diamond_df = diamond_df[(diamond_df["depth"]<75)&(diamond_df["depth"]>45)]
 diamond_df = diamond_df[(diamond_df["table"]<80)&(diamond_df["table"]>40)]  
@@ -17,10 +21,6 @@ diamond_df = diamond_df[(diamond_df["y"]<30)]
 diamond_df = diamond_df[(diamond_df["z"]<30)&(diamond_df["z"]>2)]
 
 df = diamond_df.drop(['cut','color','clarity'],axis=1)
-import random
-random.seed(90)
-diamond_df=diamond_df.loc[random.sample(list(range(len(df))),5000)].reset_index(drop=True)
-df=df.loc[random.sample(list(range(len(df))),5000)].reset_index(drop=True)
 st.subheader('The Bar plots of Diamond data')
 v1_list = ['cut','color','clarity']
 vari1 = st.selectbox(label = "Choose a First Variable", options = v1_list,key=4)
